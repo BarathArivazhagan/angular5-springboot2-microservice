8ca9b5f41eacb8878f3a0196a8250ed3a5cb9f6a
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-01-21 19:21:38 +0530

    adding bootstrap module

--- angular2-inventory-app/src/app/app.module.ts
+++ angular2-inventory-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutes} from './app.routing';
 
 
 import { AppComponent } from './app.component';
@@ -24,7 +26,9 @@ import { GetInventoryListComponent } from './get-inventory-list/get-inventory-li
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular2-inventory-app/src/app/app.routing.ts
+++ angular2-inventory-app/src/app/app.routing.ts
@@ -1,4 +1,5 @@
 import {Router, RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
 import {AddInventoryComponent} from './add-inventory/add-inventory.component';
 import {GetInventoryComponent} from './get-inventory/get-inventory.component';
 import {UpdateInventoryComponent} from './update-inventory/update-inventory.component';
@@ -7,11 +8,11 @@ import {GetInventoryListComponent} from './get-inventory-list/get-inventory-list
 
 const router: Routes = [
 
- {path: '', redirectTo: '/inventory', pathMatch: 'full'},
+ {path: '', redirectTo: '/', pathMatch: 'full'},
  {path: 'saveInventory', component: AddInventoryComponent},
  {path: 'getInventory', component: GetInventoryComponent},
  {path: 'updateInventory', component: UpdateInventoryComponent},
-  {path: 'updateInventory', component: GetInventoryListComponent},
- 
-
+ {path: 'inventories', component: GetInventoryListComponent}
 ];
+
+export const AppRoutes = RouterModule.forRoot(router);
