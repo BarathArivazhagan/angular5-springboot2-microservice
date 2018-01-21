e827cd2fab210ef8f051292bb839a27cb776da9e
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-01-21 19:38:37 +0530

    adding create inventory

--- angular2-inventory-app/src/app/add-inventory/add-inventory.component.ts
+++ angular2-inventory-app/src/app/add-inventory/add-inventory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-inventory',
@@ -10,6 +11,18 @@ export class AddInventoryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('Add Inventory Component onInit is called');
   }
+
+  submit(form: FormGroup) {
+    console.log('Template-driven form submitted: ', form.value);
+  }
+
+}
+
+interface Inventory {
+  inventoryName: string;
+  productName: string;
+  quantity: number;
 
 }
